480f114e325eb9cc3771c004e1bd4780bcefcb15
Atul Kumar Hande <handeatul03a@gmail.com>
2021-10-03 00:56:05 +0530

    propdrilling

--- src/prop-drilling.js
+++ src/prop-drilling.js
@@ -3,29 +3,39 @@ import { data } from "./data";
 
 const PropDrilling = () => {
   const [people, setPeople] = useState(data);
+  const removePerson = (id) => {
+    setPeople(people.filter((person) => person.id !== id));
+  };
 
   return (
     <div>
       <h2>Prop Drilling</h2>
-      <List people={people} />
+      <List people={people} removePerson={removePerson} />
     </div>
   );
 };
 
-const List = ({ people }) => {
+const List = ({ people, removePerson }) => {
   return (
     <>
       {people.map((person) => {
-        return <SinglePerson key={person.id} />;
+        return (
+          <SinglePerson
+            key={person.id}
+            {...person}
+            removePerson={removePerson}
+          />
+        );
       })}
     </>
   );
 };
 
-const SinglePerson = ({ id, name }) => {
+const SinglePerson = ({ id, name, removePerson }) => {
   return (
     <div className="item">
       <h4>single item</h4>
+      <button onClick={() => removePerson(id)}></button>
     </div>
   );
 };
